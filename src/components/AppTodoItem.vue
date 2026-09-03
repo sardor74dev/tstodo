40a21b716b604170621a00bc383fe29e762fdcd1
Sardor <sardor777ibragimov@gmail.com>
2026-09-03 18:33:09 +0500

    refactor: restructure app components and add styles
    - Added AppHeader, AppFilters, AppFooter, AppTodoList, AppAddTodo, and AppTodoItem components.
    - Removed unnecessary files.
    - Introduced styles.css for styling the application.

---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="todo-item" :class="{'todo-item--done' : todo.isDone}">
+        <div class="todo-item__status">
+            <i class="bi bi-check2"></i>
+        </div>
+        <span class="todo-item__text">{{ todo.text }}</span>
+        <button class="todo-item__remove-button">
+            <i class="bi bi-trash3"></i>
+        </button>
+    </li>
+</template>
+
+<script lang="ts">
+import Todo from "@/types/Todo";
+import { PropType, defineComponent } from "vue";
+
+export default defineComponent({
+    name: "AppTodoItem",
+    props: {
+        todo: {
+            type: Object as PropType<Todo>,
+            required: true
+        }
+    }
+})
+</script>
---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="todo-list">
+        <AppTodoItem v-for="todo in todos" :key="todo.id" :todo="todo" />
+    </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppTodoItem from "./AppTodoItem.vue";
+import Todo from "@/types/Todo";
+
+interface State {
+    todos: Todo[];
+}
+
+export default defineComponent({
+    name: "AppTodoList",
+    components: {
+        AppTodoItem
+    },
+    data(): State {
+        return {
+            todos: [
+                { id: 1, text: "Learn the basics of Vue", isDone: true },
+                { id: 2, text: "Learn the basics of Typescript", isDone: false },
+                { id: 3, text: "Subscribe to the channel", isDone: false },
+            ]
+        }
+    }
+})
+</script>
